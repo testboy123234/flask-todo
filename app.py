f95941f858b0d2e4e9824b5676597da813af15b7
testing doonly <106380029+DPLakmal@users.noreply.github.com>
2023-10-11 22:41:45 +0530

    Create tast.db

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Content = db.Column(db.String(200), nullable=False)
+    Completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __ref__(self):
+        return '<Task %r>' % self.id 
+    
+
+with app.app_context():
+    # Create the database tables
+    db.create_all()
 
 @app.route('/')
 def index():
